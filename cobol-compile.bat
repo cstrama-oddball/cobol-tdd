0aa1ce7f0c5459ec37398541cbba06c1e90f937c
cstrama-oddball <cstrama-oddball>
2022-11-16 11:45:40 -0500

    update scriopts

--- cobol-compile.bat
+++ cobol-compile.bat
@@ -3,7 +3,7 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`python3 gen-rand.py`) DO (
 SET val=%%F.tmp
 )
 
-python3 PreCompile.py "%cd%\%1" "%cd%\%val%" %3
+python3 PreCompile.py "%cd%\%1" "%cd%\%val%" %3 %2
 cobc %val% -o %2 %3
 rem type %val%
 del %cd%\%val%
--- cobol-compile.sh
+++ cobol-compile.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-
 val=`python3 gen-rand.py`
-python3 PreCompile.py $1 $val $3
+
+
+
+python3 PreCompile.py $1 $val $3 $2
 cobc $val -o $2 $3
 #cat $val
 rm $val
