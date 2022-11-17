97ee7451c9c8376df9bc417cb5e1126fefbf86c3
cstrama-oddball <cstrama-oddball>
2022-11-17 13:52:39 -0500

    updates

--- CWFM-source/cobol-compile.bat
+++ CWFM-source/cobol-compile.bat
@@ -2,7 +2,9 @@
 FOR /F "tokens=* USEBACKQ" %%F IN (`python3 ..\gen-rand.py`) DO (
 SET val=%%F.tmp
 )
-
+rem %1 == source file
+rem %2 == output binary
+rem %3 == -m library, -x executable
 python3 ..\PreCompile.py "%cd%\%1" "%cd%\%val%" %3 %2
 cobc %val% -o %2 %3
 rem type %val%
