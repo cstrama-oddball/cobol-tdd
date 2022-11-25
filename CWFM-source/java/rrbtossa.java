967a6acd3127069b845d9d9d676aaeff0e01e2da
cstrama-oddball <cstrama-oddball>
2022-11-25 09:39:13 -0500

    refactors

--- CWFM-source/java/rrbtossa.java
+++ CWFM-source/java/rrbtossa.java
@@ -25,21 +25,21 @@ public class rrbtossa{
 
         // reverse the order of the list to have the longest prefixes checked first, in case a shorter
         // prefix has the same start letters
-        prefix_array[14] = new PrefixSuffixConversion("A", "10", new int[] {7,10}, 991274, 994999);
-        prefix_array[13] = new PrefixSuffixConversion("H", "80", new int[] {7, 10}, 49160, 994999);
-        prefix_array[12] = new PrefixSuffixConversion("MA", "14", new int[] {8, 11}, 991274, 994999);
-        prefix_array[11] = new PrefixSuffixConversion("PA", "15", new int[] {8, 11}, 991274, 994999);
-        prefix_array[10] = new PrefixSuffixConversion("WA", "16", new int[] {8, 11}, 991274, 994999);
-        prefix_array[9] = new PrefixSuffixConversion("CA", "17", new int[] {8, 11}, 991274, 994999);
-        prefix_array[8] = new PrefixSuffixConversion("PD", "45", new int[] {8, 11}, 415936, 994999);
-        prefix_array[7] = new PrefixSuffixConversion("WD", "46", new int[] {8, 11}, 415936, 994999);
-        prefix_array[6] = new PrefixSuffixConversion("JA", "11", new int[] {8, 11}, 991274, 994999);
-        prefix_array[5] = new PrefixSuffixConversion("MH", "84", new int[] {8, 11}, 49160, 994999);
-        prefix_array[4] = new PrefixSuffixConversion("PH", "85", new int[] {8, 11}, 49160, 994999);
-        prefix_array[3] = new PrefixSuffixConversion("WH", "86", new int[] {8, 11}, 49160, 994999);
-        prefix_array[2] = new PrefixSuffixConversion("WCA", "13", new int[] {9, 12}, 991274, 994999);
-        prefix_array[1] = new PrefixSuffixConversion("WCD", "43", new int[] {9, 12}, 415936, 994999);
-        prefix_array[0] = new PrefixSuffixConversion("WCH", "83", new int[] {9}, 49160, 994999);
+        prefix_array[14] = new PrefixSuffixConversion("A",   "10", new int[] {7, 10}, 991274, 994999);
+        prefix_array[13] = new PrefixSuffixConversion("H",   "80", new int[] {7, 10},  49160, 994999);
+        prefix_array[12] = new PrefixSuffixConversion("MA",  "14", new int[] {8, 11}, 991274, 994999);
+        prefix_array[11] = new PrefixSuffixConversion("PA",  "15", new int[] {8, 11}, 991274, 994999);
+        prefix_array[10] = new PrefixSuffixConversion("WA",  "16", new int[] {8, 11}, 991274, 994999);
+        prefix_array[9]  = new PrefixSuffixConversion("CA",  "17", new int[] {8, 11}, 991274, 994999);
+        prefix_array[8]  = new PrefixSuffixConversion("PD",  "45", new int[] {8, 11}, 415936, 994999);
+        prefix_array[7]  = new PrefixSuffixConversion("WD",  "46", new int[] {8, 11}, 415936, 994999);
+        prefix_array[6]  = new PrefixSuffixConversion("JA",  "11", new int[] {8, 11}, 991274, 994999);
+        prefix_array[5]  = new PrefixSuffixConversion("MH",  "84", new int[] {8, 11},  49160, 994999);
+        prefix_array[4]  = new PrefixSuffixConversion("PH",  "85", new int[] {8, 11},  49160, 994999);
+        prefix_array[3]  = new PrefixSuffixConversion("WH",  "86", new int[] {8, 11},  49160, 994999);
+        prefix_array[2]  = new PrefixSuffixConversion("WCA", "13", new int[] {9, 12}, 991274, 994999);
+        prefix_array[1]  = new PrefixSuffixConversion("WCD", "43", new int[] {9, 12}, 415936, 994999);
+        prefix_array[0]  = new PrefixSuffixConversion("WCH", "83", new int[] {9},      49160, 994999);
     }
 
     private PrefixSuffixConversion findPrefixSuffix(String value) {
