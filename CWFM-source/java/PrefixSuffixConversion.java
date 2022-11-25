9a66fdcc60b5c2b25630902d99775bf00c44b9eb
cstrama-oddball <cstrama-oddball>
2022-11-25 09:35:45 -0500

    refactors

--- CWFM-source/java/PrefixSuffixConversion.java
+++ CWFM-source/java/PrefixSuffixConversion.java
@@ -5,18 +5,26 @@ public class PrefixSuffixConversion {
     private int _ignoreRangeLow;
     private int _ignoreRangeHigh;
 
-    private static String PREFIX_FILLER = "{00";
+    private static boolean NO_MATCH = false;
+    private static boolean MATCH = true;
+    
     private static int MIN_PREFIX_FILLER_LENGTH = 9;
+    private static int NO_PREFIX_FILLER_LENGTH = 0;
+    private static int NO_RANGE_VALUE = 0;
+    
+    private static String EMPTY_STRING = "";
+    private static String MATCHING_REGEX = "[^\\d.]";
+    private static String PREFIX_FILLER = "{00";
 
-    private String _prefixFiller = "";
-    private int _minPrefixfillerLength = 0;
+    private String _prefixFiller = EMPTY_STRING;
+    private int _minPrefixfillerLength = NO_PREFIX_FILLER_LENGTH;
 
     public PrefixSuffixConversion() {
-        _prefix = "";
-        _suffix = "";
-        _matching_len = new int[0];
-        _ignoreRangeHigh = 0;
-        _ignoreRangeLow = 0;
+        _prefix = EMPTY_STRING;
+        _suffix = EMPTY_STRING;
+        _matching_len = new int[NO_RANGE_VALUE];
+        _ignoreRangeHigh = NO_RANGE_VALUE;
+        _ignoreRangeLow = NO_RANGE_VALUE;
     }
 
     public PrefixSuffixConversion(String prefix, String suffix, int[] matching_lengths, int ignoreRangeLow, int ignoreRangeHigh) {
@@ -26,7 +34,7 @@ public class PrefixSuffixConversion {
         _ignoreRangeLow = ignoreRangeLow;
         _ignoreRangeHigh = ignoreRangeHigh;
 
-        if (matching_lengths.length > 0) {
+        if (matching_lengths.length > NO_RANGE_VALUE) {
             _prefixFiller = PREFIX_FILLER;
             _minPrefixfillerLength = MIN_PREFIX_FILLER_LENGTH;    
         }
@@ -56,37 +64,38 @@ public class PrefixSuffixConversion {
         if (value.length() <= _minPrefixfillerLength)
             return _prefixFiller;
 
-        return "";
+        return EMPTY_STRING;
     }
 
     public boolean isMatch(String value) {
-        boolean goodLen = false;
+        boolean goodLen = NO_MATCH;
 
-        String temp = value.replaceAll("[^\\d.]", "");
+        String temp = value.replaceAll(MATCHING_REGEX, EMPTY_STRING);
 
-        if (Integer.parseInt(temp) >= getIngoreRangeLow() && Integer.parseInt(temp) <= getIngoreRangeHigh()) {
-            return false;
+        if (Integer.parseInt(temp) >= getIngoreRangeLow() 
+            && Integer.parseInt(temp) <= getIngoreRangeHigh()) {
+            return NO_MATCH;
         }
 
         for (int i = 0; i < getMatchingLengths().length; i++) {
             if (getMatchingLengths()[i] == value.length()) {
-                goodLen = true;
+                goodLen = MATCH;
                 break;
             }
         }
 
         if (goodLen) {
             if (value.startsWith(getPrefix()))
-                return true;
+                return goodLen;
         }
 
-        return false;
+        return NO_MATCH;
     }
 
     public String toSSA(String value) {
-        if (_minPrefixfillerLength <= 0)
-            return "";
+        if (_minPrefixfillerLength <= NO_PREFIX_FILLER_LENGTH)
+            return EMPTY_STRING;
 
-        return getPrefixFiller(value) + value.replace(getPrefix(), "") + getSuffix();
+        return getPrefixFiller(value) + value.replace(getPrefix(), EMPTY_STRING) + getSuffix();
     }
 }
